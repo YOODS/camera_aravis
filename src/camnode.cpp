b4186b7bb3d3203b91750b9f2053b146f2c7975b
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2018-11-18 14:29:32 +0900

    Key hash algor changed

--- src/camnode.cpp
+++ src/camnode.cpp
@@ -64,7 +64,7 @@ typedef camera_aravis::CameraAravisConfig Config;
 static gboolean SoftwareTrigger_callback (void *);
 
 #include <sys/shm.h>
-static key_t shmkey=0x9761100;
+static key_t shmkey=0x39760022;
 static unsigned char *shmptr=NULL;
 
 typedef struct
@@ -900,6 +900,7 @@ int main(int argc, char** argv)
     	if (global.phNode->hasParam(ros::this_node::getName()+"/shmem")){
     		int size;
     		global.phNode->getParam(ros::this_node::getName()+"/shmem", size);
+            shmkey^=size;
             int sid=shmget(shmkey,size,IPC_CREAT|0666);
             shmptr=(unsigned char *)shmat(sid, NULL, 0);
             shmptr[0]=1;//try writing
