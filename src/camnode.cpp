4a039bb83b5df499c3592cb44a6ccc0603095c7b
Kan-Harada <harada@yoods.co.jp>
2019-01-25 13:39:55 +0900

    野村さん改修分マージ

--- src/camnode.cpp
+++ src/camnode.cpp
@@ -149,19 +149,22 @@ const char	*szBufferStatusFromInt[] = {
 										"ARV_BUFFER_STATUS_FILLING",
 										"ARV_BUFFER_STATUS_ABORTED"
 										};
-//2018.11.28 TEST start
 static void stream_cb (void *user_data, ArvStreamCallbackType type, ArvBuffer *buffer)
 {
         if (type == ARV_STREAM_CALLBACK_TYPE_INIT) {
-          if (!arv_make_thread_realtime (10))
-                printf ("Failed to make stream thread realtime\n");
-//	if (!arv_make_thread_high_priority (-10))
-//		printf ("Failed to make stream thread high priority\n");
+		if (!arv_make_thread_realtime (10)){
+			printf ("Failed to make stream thread realtime\n");
+
+			if (!arv_make_thread_high_priority (-10)){
+				printf ("Failed to make stream thread high priority\n");
+			}else{
+				printf ("Success to make stream thread high priority\n");
+			}
+		}else{
+			printf ("Success to make stream thread realtime\n");
+		}
 	}
 }
-//2018.11.28 TEST end
-
-
 
 static void set_cancel (int signal)
 {
@@ -172,7 +175,10 @@ ArvGvStream *CreateStream(void)
 {
 	gboolean 		bAutoBuffer = FALSE;
 	gboolean 		bPacketResend = TRUE;
-	unsigned int 	timeoutPacket = 1; //40 milliseconds originally
+	unsigned int 	timeoutPacket = 1; //1 milliseconds originally
+//	unsigned int 	timeoutPacket = 40;//40 milliseconds originally
+//	unsigned int 	timeoutPacket = 5; //500 microseconds originally
+//	unsigned int 	timeoutPacket = 3; //500 microseconds originally
 	unsigned int 	timeoutFrameRetention = 200;
 
 	
@@ -200,6 +206,7 @@ ArvGvStream *CreateStream(void)
 		g_object_set (pStream,
 				          "packet-timeout",
 						  (unsigned) timeoutPacket * 1000,
+//						  (unsigned) timeoutPacket * 100,
 						  "frame-retention", (unsigned) timeoutFrameRetention * 1000,
 					  NULL);
 	
@@ -419,20 +426,22 @@ void RosReconfigure_callback(Config &config, uint32_t level)
     	else
     		ROS_INFO ("Camera does not support FocusPos.");
     }
-    if (changedMtu)
-    {
-    	if (global.isImplementedMtu)
+ //   if (changedMtu)
+ //   {
+ //   	if (global.isImplementedMtu)
 		{
 			ROS_INFO ("Set mtu = %d", config.mtu);
 			arv_device_set_integer_feature_value(global.pDevice, "GevSCPSPacketSize", config.mtu);
-			arv_camera_gv_set_packet_size (global.pCamera, 9000);
+//			arv_camera_gv_set_packet_size (global.pCamera, 9000);
+//			arv_camera_gv_set_packet_size (global.pCamera, 8996);
+			arv_camera_gv_set_packet_size (global.pCamera, 8192);
 			ros::Duration(1.0).sleep();
 			config.mtu = arv_device_get_integer_feature_value(global.pDevice, "GevSCPSPacketSize");
 			ROS_INFO ("Get mtu = %d", config.mtu);
 		}
-    	else
-    		ROS_INFO ("Camera does not support mtu (i.e. GevSCPSPacketSize).");
-    }
+//    	else
+//    		ROS_INFO ("Camera does not support mtu (i.e. GevSCPSPacketSize).");
+//    }
 
     if (changedAcquisitionMode)
     {
@@ -1026,6 +1035,7 @@ fprintf(stderr,"shmem %x %dkB\n",shmkey,size>>10);
 			//arv_device_set_integer_feature_value(global.pDevice, "GainRaw", global.config.GainRaw);
 		if (global.isImplementedAcquisitionFrameRateEnable)
 			arv_device_set_integer_feature_value(global.pDevice, "AcquisitionFrameRateEnable", 1);
+
 		if (global.isImplementedAcquisitionFrameRate)
 			arv_device_set_float_feature_value(global.pDevice, global.keyAcquisitionFrameRate, global.config.AcquisitionFrameRate);
 
